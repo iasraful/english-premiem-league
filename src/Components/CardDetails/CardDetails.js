cbf9167dfd6ddb090c405764f91b7e49f186c0b1
iasraful <iasraful321@gmail.com>
2021-03-11 23:02:09 +0600

    done

--- src/Components/CardDetails/CardDetails.js
+++ src/Components/CardDetails/CardDetails.js
@@ -1,41 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import MainNavBar from "../MainNavBar/MainNavBar";
 
- import { useParams } from 'react-router';
+import { useParams } from "react-router";
 
 const CardDetails = () => {
-    const [details, setDetails] = useState([])
-    const { idTeam } = useParams();
-    
-    useEffect(()=>{
-        const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
+  const [details, setDetails] = useState([]);
+  const { idTeam } = useParams();
 
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setDetails(data.teams[0]) )
-        
-    }, [])
-     
-    const {strTeamLogo , strStadiumThumb, intFormedYear, strCountry, strSport, strGender ,strTeam}= details
+  useEffect(() => {
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
 
-    return (
-        <div className="container">
-            <div className="row bgBanner" style={{backgroundImage : `url(${strStadiumThumb})`}}>
-                <div className="col-12">
-                    <img className="teamLogo" src={strTeamLogo} alt="log"/>
-                </div>
-            </div>
-            <div className="row bg-primary my-3 py-5">
-                <div className="col-md-5">
-                    <h2>League : {strTeam} </h2>
-                    <p><strong>Founded : </strong> {intFormedYear} </p>
-                    <p><strong>Country : </strong> {strCountry} </p>
-                    <p><strong>Sport : </strong> {strSport} </p>
-                    <p><strong>Gender : </strong> {strGender} </p>
-                </div>
-            </div>
-        </div>  
-    );
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setDetails(data.teams[0]));
+  }, []);
+
+  const {
+    strTeamLogo,
+    strStadiumThumb,
+    intFormedYear,
+    strCountry,
+    strSport,
+    strGender,
+    strTeam,
+  } = details;
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <MainNavBar />
+        </div>
+      </div>
+      <div
+        className="row bgBanner"
+        style={{ backgroundImage: `url(${strStadiumThumb})` }}
+      >
+        <div className="col-12">
+          <img className="teamLogo" src={strTeamLogo} alt="log" />
+        </div>
+      </div>
+      <div className="row bg-primary my-3 py-5">
+        <div className="col-md-5">
+          <h2>League : {strTeam} </h2>
+          <p>
+            <strong>Founded : </strong> {intFormedYear}{" "}
+          </p>
+          <p>
+            <strong>Country : </strong> {strCountry}{" "}
+          </p>
+          <p>
+            <strong>Sport : </strong> {strSport}{" "}
+          </p>
+          <p>
+            <strong>Gender : </strong> {strGender}{" "}
+          </p>
+        </div>
+        <div className="col-md-7">
+          <img
+            className="img-fluid"
+            src={
+              strGender === "Male"
+                ? "https://www.dreamteamfc.com/c/wp-content/uploads/sites/4/2017/06/qatar-football.jpg?strip=all&w=750&quality=100"
+                : "https://www.thesun.co.uk/wp-content/uploads/2017/08/nintchdbpict000343303326.jpg"
+            }
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardDetails;
--- src/Components/HomeScreen/Home.js
+++ src/Components/HomeScreen/Home.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
-import CardDetails from "../CardDetails/CardDetails";
+import MainNavBar from "../MainNavBar/MainNavBar";
 
 const Home = () => {
   const [teams, setTeams] = useState([]);
   useEffect(() => {
-    fetch("https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League")
+    fetch(
+      "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League"
+    )
       .then((res) => res.json())
       .then((data) => {
         setTeams(data.teams.slice(0, 12));
-      })
+      });
   }, []);
-
-  console.log(teams); 
 
   return (
     <div>
       <div className="container">
         <div className="row">
+          <div className="col-12">
+            <MainNavBar />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div
+              style={{
+                backgroundImage:
+                  "url(https://www.snyder-associates.com/wp-content/uploads/2018/09/McFarland_Football_299_ameessmann-1-of-1-1024x600.jpg)",
+                height: "300px",
+                width: "100%",
+              }}
+            >
+              <h1 className="text-center py-5 text-white">Football Mania</h1>
+            </div>
+          </div>
+        </div>
+        <div className="row my-3">
           {teams.map((team) => (
             <div className="col-md-4 mb-3">
               <Card team={team} />
